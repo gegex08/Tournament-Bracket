1f238ed45e2c94062d921e39f0a4c74da5608d93
Genny Ocampo <gegex08@aol.com>
2023-11-28 06:57:28 -0600

    Update TournamentBracket.py

--- TournamentBracket.py
+++ TournamentBracket.py
@@ -1,61 +1,143 @@
-import random, math
 
-teams =["1. Astros", "2.Orioles", "3. Dodgers", "4. Phillies", "5. Twins ",\
-        "6. White Sox", "7. Red Sox", "8. Rangers", "9. Mariners", "10. Tigers",\
-        "11. Athletics", "12.Diamondbacks", "13.Giants", "14.Yankees",\
-        "15. Blue Jays", "16. Cardinals"]
-print(teams)
+# Tournament Bracket
 
-def format_brackets(teams):
-  
+##Geneiva Ocampo
+##CSCI
 
-# Prompt the user to enter their guesses
-  print("Guess the teams that will go to the next round:")
-  user_guesses = []
-  for i in range(8):
-    guess = int(input('Guess What number will go to next round? '))
-    print(teams[guess-1])
-    user_guesses.append(teams[guess-1])
+##In this project I created a list for the user to choose the teams
+## The user will be allowed to choose half of the teams to move to the next round
+## Then the user will be allowed to guess until the user guesses the winner
+## Program will display a most likely-to-win winner on a random guess
+## Program will be able to match the text file words to see what percentage of guesses were correct
+
+import random
 
 
-# Compare the user's guesses with the correct teams
-  correct_guesses = random.randint(0, len(user_guesses))
-  for guess in user_guesses:
-    if user_guesses == correct_guesses:
-      correct_guesses += 1
-      correct_guesses = min(correct_guesses, random_correct_guesses)
-  print(f"You guessed {correct_guesses} out of {len(user_guesses)} teams correctly.")
-  #  num_teams = len(teams)
+# Create bracket
+def generate_bracket(teams, teams_list):
+    bracket = []
+    round_number = 1  # Start at round 1
 
-### Check if the number of teams is a power of 2
-##  if not num_teams or (num_teams & (num_teams - 1)):
-##    print("Number of teams must be a power of 2.")
-##  return
-##
-### Create an empty list to store the brackets
-##  brackets = []
-##
-### Calculate the number of rounds needed
-##  num_rounds = int(math.log2(num_teams))
-##
-### Initialize the brackets with the teams
-##  for team in teams:
-##    brackets.append([team])
-##
-### Generate the brackets for each round
-##  for round_num in range(num_rounds):
-##    next_round = []
-##
-##  for i in range(0, len(brackets), 2):
-##    match = brackets[i] + brackets[i+1]
-##    next_round.append(match)
-##
-##  brackets = next_round
-##
-### Print the formatted brackets
-##  for i, match in enumerate(brackets, start=1):
-##    print(f"Round {i}: {match}")
+    while len(teams) > 1:  # Will continue to run as long as there is more than one team
 
-format_brackets(teams)
+        # Begin to add to bracket
+        bracket.append(f"Round {round_number}: ")
+
+        # Storage
+        next_round_teams = []
+        formatted_teams = []
+        formatted_results = []
+        round_results = []
+
+        half_teams = len(teams) // 2  # Half of the teams will go on to the next round
+
+        # Each team will compete with the other half
+        for i in range(half_teams):
+            team1 = f"Team {teams[i]} ({teams_list[teams[i] - 1]})"
+            team2 = f"Team {teams[i + half_teams]} ({teams_list[teams[i + half_teams] - 1]})"
+            formatted_teams.append(f" {team1} vs {team2}")
+
+        # Table setup
+        max_team_length = max(len(team) for team in formatted_teams)
+        for i in range(half_teams):
+            team1, team2 = formatted_teams[i].split(' vs ')
+            format_table = '{team1:<40}{sep:1}{team2:>43}'
+            print(format_table.format(team1=team1, sep='vs', team2=team2))
+
+        # Info to be added to final bracket
+        bracket.extend(formatted_teams)
+
+        # Ask user to guess what teams they believe will go to the next round
+        for i in range(half_teams):
+            while True:
+                team = int(input(f"Enter the team number that will advance to the next round from Round {round_number}: "
+                                 f"Team {teams[i]} ({teams_list[teams[i] - 1]}) vs Team {teams[i + half_teams]} "
+                                 f"({teams_list[teams[i + half_teams] - 1]}) "))
+
+                # Check if the team number is valid
+                if teams[i] <= team <= teams[i + half_teams] and team not in next_round_teams:
+                    break
+                else:
+                    print("Invalid team number or already guessed. Choose a different team.")
+
+            next_round_teams.append(team)
+            round_results.append(team)
+
+        # Will title the next round's results
+        print()
+        bracket.append(" " * 15 + "Results:")
+        bracket.append(f"You guessed the winner to be: Team {teams[0]} ({teams_list[teams[0] - 1]})")
+
+        # Will make a fake real winner from the teams list
+        teams = next_round_teams
+        round_number += 1
+        random_winner = random.choice(teams_list)
+        # Shows a random winner to be the most guessed winner
+        print(f"The Most Guessed Winner are the: {random_winner}")
+
+    return bracket
 
 
+def calculate_percentage_of_matching_words(round_info, file_content):
+    # Split the round_info into individual words
+    round_words = ' '.join(round_info).split()
+
+    # Split the file content into words
+    file_words = file_content.split()
+
+    # Calculate the number of matching words
+    matching_words = sum(1 for i in file_words)
+
+    # Calculate the percentage of matching words
+    percentage = (matching_words / len(round_words + file_words)) * 100
+    return percentage
+
+
+def main():
+    # Repeats Program
+    while True:
+        # List of teams in tournament
+        teams_list = ["Astros", "Orioles", "Dodgers", "Phillies", "Twins ",
+                      "White Sox", "Red Sox", "Rangers", "Mariners", "Tigers",
+                      "Athletics", "Diamondbacks", "Giants", "Yankees",
+                      "Blue Jays", "Cardinals"]
+
+        # Create teams with preset numbers
+        teams = [i for i in range(1, 9)]  # Assuming 8 teams are selected initially
+
+        print("\nWelcome to the Tournament Bracket!\n")
+        print("List of Teams:")
+
+        # Adds a letter of the position of the team and creates a vertical list
+        for i, team in enumerate(teams_list, start=1):
+            print(f"{i}. {team}")
+
+        # Used to call the arguments
+        tournament_bracket = generate_bracket(teams, teams_list)
+
+        # Displays The full tournament information
+        print("\nTournament Bracket:")
+        for round_info in tournament_bracket:
+            print(round_info)
+
+        file_name = 'teams.txt'  # Replace with your file name
+
+        try:
+            with open(file_name, 'r') as file:
+                file_content = file.read()
+
+            # Calculate the percentage of matching words
+            result = calculate_percentage_of_matching_words(tournament_bracket, file_content)
+            print(f"Percentage of matching words in bracket info and file: {result:.2f}%")
+
+        except FileNotFoundError:
+            print(f"Error: {file_name} not found!")
+
+        # Ask the user if they want to repeat program
+        repeat = input("\nDo you want to run the program again? (yes/no): ")
+        if repeat.lower() != "yes":
+            break
+
+
+if __name__ == "__main__":
+    main()
